b20e2f0aeaac438f1197a2e926abd11021591cf0
SIKASZZU <soots.sigvrid@gmail.com>
2025-01-04 17:08:00 +0200

    resource_path func

--- src/images.py
+++ src/images.py
@@ -1,8 +1,11 @@
 import pygame
+import os
+import sys
 
 class Images:
     def __init__(self):
         self.loaded_item_images = {}
+
 
     def preloading(self, image_name, image_path):
 
@@ -18,7 +21,8 @@ class Images:
             converted_image = loaded_image.convert_alpha()
             self.loaded_item_images[image_name] = converted_image
             return converted_image
-                
+
+
     def _load_and_convert_image(self, image_path: str, image_name: str):
         """Utility method to load and convert images."""
         try:
@@ -28,3 +32,9 @@ class Images:
             return converted_image
         except pygame.error:
             return None
+
+
+    def resource_path(self, relative_path):
+        """ Get absolute path to resource, works for dev and for PyInstaller """
+        base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
+        return os.path.join(base_path, relative_path)
--- src/tree.py
+++ src/tree.py
@@ -10,7 +10,7 @@ class Tree:
         
         self.resource_value = (50, 120)
 
-        path = 'res/images/snowy_tree.png'
+        path = self.images.resource_path('res/images/snowy_tree.png')
         self.img = self.images.preloading('tree', path)
         self.image = pygame.transform.scale(self.img, (self.width, self.height))
         

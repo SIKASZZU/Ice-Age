de3a9ff3b28985560a64a0a79ef083a0474f9049
Oljake <109909586+Oljake@users.noreply.github.com>
2025-01-22 17:56:39 +0200

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,57 +1,64 @@
 import socket
 from _thread import *
-from test_player import Player
+from player import Player
 import pickle
 
 server = "0.0.0.0"
-port = 27015  # Default 27015
+port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((server, port))
-
 
 try:
     s.bind((server, port))
 except socket.error as e:
-    str(e)
+    print(str(e))
 
-s.listen(2)
+s.listen()
 print("Waiting for a connection, Server Started")
 
+# Track connected players dynamically
+players = {}
+player_id_counter = 0
+lock = allocate_lock()  # To prevent race conditions when updating players
 
-players = [Player(0,0,50,50,(255,0,0)), Player(100,100, 50,50, (0,0,255))]
 
-def threaded_client(conn, player):
-    conn.send(pickle.dumps(players[player]))
-    reply = ""
-    while True:
-        try:
+def threaded_client(conn, player_id):
+    global players, player_id_counter
+    try:
+        # Send player ID and initial players list
+        conn.send(pickle.dumps({"player_id": player_id, "players": list(players.values())}))
+
+        while True:
             data = pickle.loads(conn.recv(2048))
-            players[player] = data
-
             if not data:
-                print("Disconnected")
                 break
-            else:
-                if player == 1:
-                    reply = players[0]
-                else:
-                    reply = players[1]
 
-                print("Received: ", data)
-                print("Sending : ", reply)
+            # Update the current player's data
+            with lock:
+                players[player_id] = data
 
-            conn.sendall(pickle.dumps(reply))
-        except:
-            break
+            # Send all active players back
+            with lock:
+                conn.sendall(pickle.dumps(list(players.values())))
+    except:
+        pass
+    finally:
+        # Remove player when they disconnect
+        with lock:
+            print(f"Player {player_id} disconnected")
+            del players[player_id]
+            player_id_counter -= 1
 
-    print("Lost connection")
-    conn.close()
+        conn.close()
 
-currentPlayer = 0
+
 while True:
     conn, addr = s.accept()
-    print("Connected to:", addr)
+    print(f"Connected to: {addr}")
 
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+    with lock:
+        player_id = player_id_counter
+        players[player_id] = Player(50 * player_id, 50 * player_id, 50, 50, (255, 0, 0))  # Example starting position
+        player_id_counter += 1
+
+    start_new_thread(threaded_client, (conn, player_id))

18d2e348a5285044fb5bd8c357385d9c9a338b73
Oljake <olariviitak@gmail.com>
2025-03-04 23:08:18 +0200

    Camera - NEW FUNCS -> click_to_world_coordinates // click_to_world_grid

--- src/camera.py
+++ src/camera.py
@@ -2,7 +2,8 @@ import pygame
 
 
 class Camera:
-    def __init__(self):
+    def __init__(self, game_instance):
+        self.game = game_instance
         self.display_surface = pygame.display.get_surface()
         self.offset = pygame.math.Vector2()
 
@@ -22,6 +23,35 @@ class Camera:
         self.offset.x = target.centerx - self.half_w
         self.offset.y = target.centery - self.half_h
 
+    def click_to_world_coordinate(self, player_rect_center, mouse_pos):
+        """
+        Converts mouse position to world coordinates.
+
+        player_rect_center -> self.player.rect.center,
+        mouse_pos -> pygame.mouse.get_pos(),
+
+        Returns world_x, world_y -> Coordinate position
+        """
+
+        world_x = player_rect_center[0] + (mouse_pos[0] - self.game.screen_x // 2)
+        world_y = player_rect_center[1] + (mouse_pos[1] - self.game.screen_y // 2)
+        return world_x, world_y
+
+    def click_to_world_grid(self, player_rect_center, mouse_pos, tile_size):
+        """
+        Converts mouse position to world grid coordinates.
+
+        player_rect_center -> self.player.rect.center,
+        mouse_pos -> pygame.mouse.get_pos(),
+        tile_size -> self.map.tile_size,
+
+        Returns world_grid_x, world_grid_y -> Grid position
+        """
+
+        world_grid_x = (player_rect_center[0] + (mouse_pos[0] - self.game.screen_x // 2)) // tile_size
+        world_grid_y = (player_rect_center[1] + (mouse_pos[1] - self.game.screen_y // 2)) // tile_size
+
+        return world_grid_x, world_grid_y
 
     def update(self, player):
         self.center_target_camera(player)
